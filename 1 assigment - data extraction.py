4749603b2b42d7a1d65453f5a5563a4c40537495
l3paulina <lesci.paulina@gmail.com>
2025-03-24 16:26:38 +0200

    Add files via upload

---
+++ 1 assigment - data extraction.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from zipfile import ZipFile
+import pandas as pd
+
+def download_and_extract_zip(url, extract_to="ais_data"):
+    zip_path = "ais_data.zip"
+    
+    print(f"Downloading data from {url}...")
+    response = requests.get(url, stream=True)
+    with open(zip_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            f.write(chunk)
+    
+    print("Extracting data...")
+    with ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+    os.remove(zip_path)
+    return extract_to
+
+def load_and_save_data_to_csv(folder, output_file="ais_data.csv"):
+
+    file_path = os.path.join(folder, "aisdk-2024-03-19.csv") 
+    print(f"Loading data from {file_path}...")
+
+    data = pd.read_csv(file_path)
+    print(f"Saving data to {output_file}...")
+
+    data.to_csv(output_file, index=False)
+    print(f"Data saved to {output_file}")
+
+file_url = 'https://web.ais.dk/aisdata/aisdk-2024-03-19.zip'
+extracted_folder = download_and_extract_zip(file_url)
+csv_file = load_and_save_data_to_csv("ais_data", output_file="ais_data.csv")
